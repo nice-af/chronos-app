c45a512bb4ccdc2980722590163ca0a35e392e90
Adrian Fahrbach <adrian@hatchery.io>
2023-10-20 00:05:32 +0200

    ✨ feat: add pressable test

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 function App(): JSX.Element {
   // const isDarkMode = useColorScheme() === 'dark';
+  const [test, setTest] = useState(false);
 
   return (
     <View>
-      <Text style={styles.text}>Hello World</Text>
+      <Pressable onPress={() => setTest(!test)}>
+        <Text style={test ? styles.text1 : styles.text2}>Hello World</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
+  text1: {
     color: 'red',
+  },
+  text2: {
+    color: 'blue',
   },
 });
 
