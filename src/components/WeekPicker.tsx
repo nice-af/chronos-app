59c3613799219da0a11d655bd4bdc5baf2daa6b2
Adrian Fahrbach <adrian@hatchery.io>
2023-11-20 23:38:44 +0100

    ✨ feat: add functionality to week picker

--- src/components/WeekPicker.tsx
+++ src/components/WeekPicker.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import { getISOWeek } from 'date-fns';
+import React, { useContext } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import { GlobalContext } from '../contexts/global.context';
 import { colors } from '../styles/colors';
 import { typo } from '../styles/typo';
 import { ButtonTransparent } from './ButtonTransparent';
 
 export const WeekPicker: React.FC = () => {
+  const { selectedDate, setSelectedDate } = useContext(GlobalContext);
   return (
-    <View style={styles.container}>
-      <ButtonTransparent onPress={() => {}} hasLargePadding>
-        <Image style={styles.arrow} source={require('../assets/icon-chevron-left.png')} />
-      </ButtonTransparent>
-      <View>
-        <Text style={styles.label}>KW</Text>
-        <Text style={styles.value}>41</Text>
+    <>
+      <View style={styles.container}>
+        <ButtonTransparent
+          onPress={() => {
+            const newDate = new Date(selectedDate);
+            newDate.setDate(newDate.getDate() - 7);
+            setSelectedDate(newDate);
+          }}
+          hasLargePadding>
+          <Image style={styles.arrow} source={require('../assets/icon-chevron-left.png')} />
+        </ButtonTransparent>
+        <View>
+          <Text style={styles.label}>KW</Text>
+          <Text style={styles.value}>{getISOWeek(selectedDate)}</Text>
+        </View>
+        <ButtonTransparent
+          onPress={() => {
+            const newDate = new Date(selectedDate);
+            newDate.setDate(newDate.getDate() + 7);
+            setSelectedDate(newDate);
+          }}
+          hasLargePadding>
+          <Image style={styles.arrow} source={require('../assets/icon-chevron-right.png')} />
+        </ButtonTransparent>
       </View>
-      <ButtonTransparent onPress={() => {}} hasLargePadding>
-        <Image style={styles.arrow} source={require('../assets/icon-chevron-right.png')} />
-      </ButtonTransparent>
-    </View>
+    </>
   );
 };
 
@@ -36,9 +53,11 @@ const styles = StyleSheet.create({
   label: {
     ...typo.subheadline,
     color: colors.textSecondary,
+    textAlign: 'center',
   },
   value: {
     ...typo.headline,
     color: colors.textPrimary,
+    textAlign: 'center',
   },
 });
