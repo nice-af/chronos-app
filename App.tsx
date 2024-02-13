0ae7f4c52970cc01455ebb30684f4bfad7984159
Adrian Fahrbach <adrian@hatchery.io>
2024-02-13 23:15:55 +0100

    ✨ feat: add TextInput to App.tsx

--- App.tsx
+++ App.tsx
@@ -2,17 +2,15 @@ import React, { FC, Suspense } from 'react';
 import { DebugTools } from './src/components/DebugTools';
 import { GlobalProvider } from './src/providers/GlobalProvider';
 import { Main } from './src/screens/Main';
-import { Text } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 const App: FC = () => {
   return (
     // TODO @AdrianFahrbach make pretty
-    <Suspense fallback={<Text>Loading...</Text>}>
-      <GlobalProvider>
-        <Main />
-        {__DEV__ && <DebugTools />}
-      </GlobalProvider>
-    </Suspense>
+    <View style={{ marginTop: 100, padding: 20 }}>
+      <Text>Input</Text>
+      <TextInput style={{ backgroundColor: 'grey' }} />
+    </View>
   );
 };
 
