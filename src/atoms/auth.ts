281ab58a8b14300bffac8d123e99e1657c406d9f
florianmrz <florian.merz@mail.de>
2024-02-25 20:52:47 +0100

    🐛 fix: clear worklogs upon logout

--- src/atoms/auth.ts
+++ src/atoms/auth.ts
@@ -1,12 +1,15 @@
 import { Version3Models } from 'jira.js';
 import { atom } from 'jotai';
 import { AuthModel } from '../services/storage.service';
+import { worklogsLocalAtom, worklogsRemoteAtom } from './worklog';
 
 export const jiraAuthAtom = atom<AuthModel | null>(null);
 
 export const logoutAtom = atom(null, (_get, set) => {
   set(jiraAuthAtom, null);
   set(userInfoAtom, null);
+  set(worklogsLocalAtom, []);
+  set(worklogsRemoteAtom, []);
 });
 
 export const userInfoAtom = atom<Version3Models.User | null>(null);
