57e66d0b0da88d90aef4e6dc7d8e96c43a56abd6
Adrian Fahrbach <adrian@hatchery.io>
2024-05-08 19:18:15 +0200

    🐛 fix: empty local id in worklog comments

--- src/services/atlassian-document-format.service.ts
+++ src/services/atlassian-document-format.service.ts
@@ -7,6 +7,25 @@ export function convertAdfToMd(adf: Document) {
   return convert(adf as any);
 }
 
+/**
+ * This function removes the localId attribute recursively if it is null
+ */
+function removeLocalIdIfNull(commentData: Document) {
+  if (commentData.content) {
+    commentData.content.forEach(content => {
+      if (content.attrs && content.attrs.localId === null) {
+        delete content.attrs.localId;
+      }
+      if (content.attrs && Object.keys(content.attrs).length === 0) {
+        delete content.attrs;
+      }
+      if (content.content) {
+        removeLocalIdIfNull(content as Document);
+      }
+    });
+  }
+}
+
 export function convertMdToAdf(markdown: string): Document {
   const transformer = new MarkdownTransformer(defaultSchema);
   const topLevelNode = transformer.parse(markdown);
@@ -14,17 +33,9 @@ export function convertMdToAdf(markdown: string): Document {
 
   // The ADF MarkdownTransformer sometimes adds a localId attribute with a null value
   // This is not valid ADF and will cause the API to return a 400 error
-  if (commentData.content) {
-    commentData.content.forEach(content => {
-      if (content.attrs && content.attrs.localId === null) {
-        delete content.attrs.localId;
-      }
-      // Remove attrs if empty
-      if (content.attrs && Object.keys(content.attrs).length === 0) {
-        delete content.attrs;
-      }
-    });
-  }
+  removeLocalIdIfNull(commentData);
+
+  console.log({ ...commentData, version: 1 });
 
   return { ...commentData, version: 1 };
 }
