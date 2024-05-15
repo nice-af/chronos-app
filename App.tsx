0069e255d11b8397c2131ef23c681585cfa31383
Adrian Fahrbach <adrian@hatchery.io>
2024-05-15 22:52:49 +0200

    🧹 chore: code cleanup

--- App.tsx
+++ App.tsx
@@ -4,21 +4,17 @@ import { Text } from 'react-native';
 import { store } from './src/atoms';
 import { ColorSchemeWatcher } from './src/components/ColorSchemeWatcher';
 import { DebugTools } from './src/components/DebugTools';
+import { Modal } from './src/components/Modal';
 import { WorklogStateWatcher } from './src/components/WorklogStateWatcher';
 import { GlobalProvider } from './src/providers/GlobalProvider';
 import { Main } from './src/screens/Main';
-import { Modal } from './src/components/Modal';
 
 const App: FC = () => {
   return (
     // TODO @AdrianFahrbach make pretty
     <Suspense fallback={<Text>Loading...</Text>}>
       <Provider store={store}>
-        <Modal
-        // icon='timer-warning'
-        // headline='You are trying to start a timer on a passed day'
-        // text='Are you sure that you want to continue? Otherwise the timer won’t be started.'
-        />
+        <Modal />
         <WorklogStateWatcher />
         <ColorSchemeWatcher />
         <GlobalProvider>
