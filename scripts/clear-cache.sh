3a4be43624b75c093d5c8b486dce8d0c06f7c669
Adrian Fahrbach <adrianfahrbach@me.com>
2025-06-21 00:01:40 +0200

    chore: adjust clear cache script

--- scripts/clear-cache.sh
+++ scripts/clear-cache.sh
@@ -12,14 +12,6 @@ rm -rf node_modules package-lock.json Pods Podfile.lock Gemfile.lock
 # Install gems
 bundle install
 
-# Clean ios folder
-cd ios
-rm -rf ./build Podfile.lock
-pod deintegrate
-pod cache clean --all
-pod setup
-cd ..
-
 # Clean macos folder
 cd macos
 rm -rf ./build Podfile.lock
@@ -30,7 +22,6 @@ cd ..
 
 # Reinstall dependencies
 npm i
-npx pod-install ios
 npx pod-install macos
 
 watchman shutdown-server
