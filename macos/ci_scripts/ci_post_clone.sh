233d11d759388e1ee93776fc77936ab4bd5e3015
Adrian Fahrbach <adrianfahrbach@me.com>
2025-07-27 22:07:56 +0200

    chore: adjust build script to also link node

--- macos/ci_scripts/ci_post_clone.sh
+++ macos/ci_scripts/ci_post_clone.sh
@@ -5,6 +5,7 @@ export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
 echo "Installing Node.js..."
 brew install node@22
+brew link node@22
 
 # Install dependencies
 echo "Installing dependencies..."
