555cbd46a798b493a3ac47374f3f6c93ee599014
Adrian Fahrbach <adrianfahrbach@me.com>
2025-07-27 22:43:08 +0200

    ci: add manual symlinks to node

--- macos/ci_scripts/ci_post_clone.sh
+++ macos/ci_scripts/ci_post_clone.sh
@@ -3,9 +3,13 @@
 echo "Installling CocoaPods..."
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+
 echo "Installing Node.js..."
 brew install node@22
-brew link node@22
+echo 'export PATH="/usr/local/opt/node@22/bin:$PATH"' >> ~/.zshrc
+export LDFLAGS="-L/usr/local/opt/node@22/lib"
+export CPPFLAGS="-I/usr/local/opt/node@22/include"
+ln -s $(which node) /usr/local/bin/node
 
 # Install dependencies
 echo "Installing dependencies..."
