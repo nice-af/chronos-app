b6e41295f9c058938813036619ab131becf23ffa
Adrian Fahrbach <adrianfahrbach@me.com>
2025-08-03 12:18:23 +0200

    chore: remove debugging setting from newer rn version

--- index.js
+++ index.js
@@ -5,7 +5,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { NativeModules } from 'react-native';
 
-NativeModules.DevSettings.setIsDebuggingRemotely(false);
 AppRegistry.registerComponent(appName, () => App);
