fa7e19ee395424815b425d6cff452d4af82dd9c2
Adrian Fahrbach <adrianfahrbach@me.com>
2025-08-03 13:06:26 +0200

    chore: adjust release it script to sync versions across targets

--- scripts/release-it/xcode.mjs
+++ scripts/release-it/xcode.mjs
@@ -15,39 +15,56 @@ class XCode extends Plugin {
     const day = today.getDate().toString().padStart(2, '0');
     const buildNumber = `${year}${month}${day}`;
 
-    // Get CFBundleVersion from the Info.plist file
-    let infoPlistFile = fs.readFileSync('./macos/Chronos-macOS/Info.plist', 'utf8');
-    const currentBuildNumber = infoPlistFile.match(/<key>CFBundleVersion<\/key>\n\t<string>(.*)<\/string>/)[1];
+    const targets = [
+      {
+        name: 'Chronos-macOS',
+        infoPlistPath: './macos/Chronos-macOS/Info.plist',
+      },
+      {
+        name: 'Chronos Companion',
+        infoPlistPath: './macos/Chronos Companion/Info.plist',
+      },
+      {
+        name: 'ChronosWidget4WeeksOverview',
+        infoPlistPath: './macos/ChronosWidget4WeeksOverview/Info.plist',
+      },
+    ];
 
-    // The buildNumber should be in the format YYMMDD.
-    // If there already is a build number for today, we add a ".1" to it
-    // If there already is a build number with a suffix, we increment the number
-    let newBuildNumber = buildNumber;
-    if (currentBuildNumber === buildNumber) {
-      newBuildNumber = `${buildNumber}.1`;
-    } else if (currentBuildNumber.startsWith(buildNumber)) {
-      const suffix = currentBuildNumber.slice(buildNumber.length);
-      const suffixNumber = parseInt(suffix);
-      newBuildNumber = `${buildNumber}.${suffixNumber + 1}`;
-    }
+    targets.forEach(target => {
+      // Get CFBundleVersion from the Info.plist file
+      let infoPlistFile = fs.readFileSync(target.infoPlistPath, 'utf8');
+      const currentBuildNumber = infoPlistFile.match(/<key>CFBundleVersion<\/key>\n\t<string>(.*)<\/string>/)[1];
 
-    // Update the CFBundleShortVersionString and CFBundleVersion in the Info.plist file
-    infoPlistFile = infoPlistFile
-      .replace(
-        /<key>CFBundleShortVersionString<\/key>\n\t<string>.*<\/string>/,
-        `<key>CFBundleShortVersionString</key>\n\t<string>${version}</string>`
-      )
-      .replace(
-        /<key>CFBundleVersion<\/key>\n\t<string>.*<\/string>/,
-        `<key>CFBundleVersion</key>\n\t<string>${newBuildNumber}</string>`
-      );
-    fs.writeFileSync('./macos/Chronos-macOS/Info.plist', infoPlistFile);
+      // The buildNumber should be in the format YYMMDD.
+      // If there already is a build number for today, we add a ".1" to it
+      // If there already is a build number with a suffix, we increment the number
+      let newBuildNumber = buildNumber;
+      if (currentBuildNumber === buildNumber) {
+        newBuildNumber = `${buildNumber}.1`;
+      } else if (currentBuildNumber.startsWith(buildNumber)) {
+        const suffix = currentBuildNumber.slice(buildNumber.length);
+        const suffixNumber = parseInt(suffix);
+        newBuildNumber = `${buildNumber}.${suffixNumber + 1}`;
+      }
+
+      // Update the CFBundleShortVersionString and CFBundleVersion in the Info.plist file
+      infoPlistFile = infoPlistFile
+        .replace(
+          /<key>CFBundleShortVersionString<\/key>\n\t<string>.*<\/string>/,
+          `<key>CFBundleShortVersionString</key>\n\t<string>${version}</string>`
+        )
+        .replace(
+          /<key>CFBundleVersion<\/key>\n\t<string>.*<\/string>/,
+          `<key>CFBundleVersion</key>\n\t<string>${newBuildNumber}</string>`
+        );
+      fs.writeFileSync(target.infoPlistPath, infoPlistFile);
+    });
 
     // Update the MARKETING_VERSION and CURRENT_PROJECT_VERSION in the xcodeproj file
     let xcodeprojFile = fs.readFileSync('./macos/Chronos.xcodeproj/project.pbxproj', 'utf8');
     xcodeprojFile = xcodeprojFile
       .replace(/MARKETING_VERSION = [0-9]+\.[0-9]+\.[0-9]+;/g, `MARKETING_VERSION = "${version}";`)
-      .replace(/CURRENT_PROJECT_VERSION = .*;/g, `CURRENT_PROJECT_VERSION = ${newBuildNumber};`);
+      .replace(/CURRENT_PROJECT_VERSION = .*;/g, `CURRENT_PROJECT_VERSION = ${buildNumber};`);
     fs.writeFileSync('./macos/Chronos.xcodeproj/project.pbxproj', xcodeprojFile);
   }
 }
