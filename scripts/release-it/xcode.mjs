60b14b8947fbfd0d0e58eaff72ab5d7344c33f8e
Adrian Fahrbach <adrianfahrbach@me.com>
2025-10-03 00:42:58 +0200

    chore: revert xcode project changes

--- scripts/release-it/xcode.mjs
+++ scripts/release-it/xcode.mjs
@@ -7,32 +7,6 @@ class XCode extends Plugin {
     return packageJson.version;
   }
 
-  updateInfoPlist(infoPlistPath, version, buildNumber) {
-    let infoPlistFile = fs.readFileSync(infoPlistPath, 'utf8');
-    const currentBuildNumber = infoPlistFile.match(/<key>CFBundleVersion<\/key>\n\s+<string>(.*)<\/string>/)?.[1];
-
-    let newBuildNumber = buildNumber;
-    if (currentBuildNumber === buildNumber) {
-      newBuildNumber = `${buildNumber}.1`;
-    } else if (currentBuildNumber?.startsWith(buildNumber)) {
-      const suffix = currentBuildNumber.slice(buildNumber.length);
-      const suffixNumber = parseInt(suffix);
-      newBuildNumber = `${buildNumber}.${suffixNumber + 1}`;
-    }
-
-    infoPlistFile = infoPlistFile
-      .replace(
-        /<key>CFBundleShortVersionString<\/key>\n(\s+)<string>.*<\/string>/,
-        `<key>CFBundleShortVersionString</key>\n$1<string>${version}</string>`
-      )
-      .replace(
-        /<key>CFBundleVersion<\/key>\n(\s+)<string>.*<\/string>/,
-        `<key>CFBundleVersion</key>\n$1<string>${newBuildNumber}</string>`
-      );
-
-    fs.writeFileSync(infoPlistPath, infoPlistFile);
-  }
-
   updateProjectPbxproj(xcodeprojPath, version, buildNumber) {
     let xcodeprojFile = fs.readFileSync(xcodeprojPath, 'utf8');
     xcodeprojFile = xcodeprojFile
@@ -48,7 +22,6 @@ class XCode extends Plugin {
     const month = (today.getMonth() + 1).toString().padStart(2, '0');
     const day = today.getDate().toString().padStart(2, '0');
     const buildNumber = `${year}${month}${day}`;
-    this.updateInfoPlist('./macos/Chronos-macOS/Info.plist', version, buildNumber);
     this.updateProjectPbxproj('./macos/Chronos.xcodeproj/project.pbxproj', version, buildNumber);
   }
 }
